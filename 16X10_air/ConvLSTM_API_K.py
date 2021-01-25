18173cbef26b9a497c23f1e0f960eb49ee227cdc
drsarwar <63023354+drsarwar@users.noreply.github.com>
2021-01-25 10:36:20 -0800

    modified ConvLSTM to work with updated data read script

--- 16X10_air/ConvLSTM_API_K.py
+++ 16X10_air/ConvLSTM_API_K.py
@@ -33,6 +33,7 @@ from keras.layers import BatchNormalization
 from keras.layers import ConvLSTM2D
 
 
+
 def extract_data(d_file):
     file=open(d_file, 'r')
     lines=file.readlines()
@@ -54,17 +55,19 @@ def extract_data(d_file):
     baseline=np.mean(data_raw[0:3,:],axis=0)
     data=data_raw-baseline
     
+    window=5
     ext=[]
-    g_data=data[0:5,:].reshape(1,data.shape[1],5)
+    g_data=np.transpose(data[0:window,:])
+    #g_data=data[0:window,:].reshape(1,data.shape[1],window)
     crop=data[0,:].reshape(1,-1)
     signal_cnt=0;
     base_cnt=0;
-    for j in range(data.shape[0]): #loop through t in data
+    for frm in range(data.shape[0]): #loop through frames aka time in data
         cnt=0
         base_flag=False;
         signal_flag=True;
-        for k in range(data.shape[1]): #loop through taxels in data
-            if (np.abs((data_raw[j,k]-baseline[k]))<3): #this is the threshold, under this it means 
+        for txl in range(data.shape[1]): #loop through taxels in data
+            if (np.abs((data[frm,txl]))<4): #this is the threshold, under this it means 
                 cnt=cnt+1                           #that the signal is baseline
                 if (cnt==160):
                     base_flag=True;
@@ -79,47 +82,55 @@ def extract_data(d_file):
             signal_flag=True;
             signal_cnt=signal_cnt+1
             base_cnt=0
-            crop=np.append(crop,data[j,:].reshape(1,-1),axis=0)
+            crop=np.append(crop,data[frm,:].reshape(1,-1),axis=0)
+            crop_t=np.transpose(crop)
+        if ((signal_flag==True) and (signal_cnt>window)):
+            g_data=np.dstack((g_data,crop_t[:,-window:]))
     
-        if ((signal_flag)&(signal_cnt>4)):
-            g_data=np.append(g_data,crop[-5:,:].reshape(1,data.shape[1],-1),axis=0)
+    g2_data=np.zeros(shape=(g_data.shape[2],window,16,10))
     
-    g_data_scaled=g_data/512
-    g_data_2D=g_data_scaled.reshape(g_data.shape[0],g_data.shape[2],16,10)    
+    for n_win in range(g_data.shape[2]):
+        for n_frm in range(window):
+            for y_frm in range(16):
+                for x_frm in range(10):
+                    g2_data[n_win,n_frm,y_frm,x_frm]=g_data[(x_frm+10*y_frm),n_frm,n_win]
+    
+    g_data_2D=g2_data/256
+    #g_data_2D=g_data_scaled.reshape(g_data.shape[0],16,10,g_data.shape[2])    
     
     return(crop,g_data,g_data_2D)
 
 def extract_baseline(d_file):
-    file=open(d_file, 'r')
-    lines=file.readlines()
+    file_b=open(d_file, 'r')
+    lines_b=file_b.readlines()
     
-    data_lis=[]
+    data_lis_b=[]
     
-    for l in range(len(lines)):
-        s=lines[l].replace(',,',',').split(',')
-        for i in range(len(s)-1):
-            if (s[i]==""):
-                del s[i]
-            elif (s[i]=="\n"):
-                del s[i]
-        nn=(list(map(int,s)))
-        if (len(nn)==160):
-            data_lis.append(nn)
+    for l in range(len(lines_b)):
+        s_b=lines_b[l].replace(',,',',').split(',')
+        for i in range(len(s_b)-1):
+            if (s_b[i]==""):
+                del s_b[i]
+            elif (s_b[i]=="\n"):
+                del s_b[i]
+        nn_b=(list(map(int,s_b)))
+        if (len(nn_b)==160):
+            data_lis_b.append(nn_b)
     
-    data_raw=np.array(data_lis)
-    baseline=np.mean(data_raw[0:3,:],axis=0)
-    data=data_raw-baseline
+    data_raw_b=np.array(data_lis_b)
+    baseline_b=np.mean(data_raw_b[0:3,:],axis=0)
+    data_b=data_raw_b-baseline_b
     
-    b_data=data[0:5,:].reshape(1,data.shape[1],5)
-    for i in range(data.shape[0]-5):
-        b_data=np.append(b_data,data[i:i+5,:].reshape(1,data.shape[1],-1),axis=0)
+    b_data=data_b[0:5,:].reshape(1,data_b.shape[1],5)
+    for i in range(data_b.shape[0]-5):
+        b_data=np.append(b_data,data_b[i:i+5,:].reshape(1,data_b.shape[1],-1),axis=0)
     
-    b_data_scaled=b_data/512
+    b_data_scaled=b_data/256
     b_data_2D=b_data_scaled.reshape(b_data.shape[0],b_data.shape[2],16,10)    
    
     return (b_data,b_data_2D)
 
-
+#paths for train data
 baseline_file='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/baseline.txt'
 air_stroke_file='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/air_stroke.txt'
 light_stroke_file='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/light_stroke.txt'
@@ -128,11 +139,15 @@ tickle_file='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/tickle.tx
 hit_file='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/hit.txt'
 
 #paths for test data
-#baseline_test_file='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10/baseline_test.txt'
-#stroke_test_file='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10/stroke_test.txt'
-#massage_test_file='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10/massage_test.txt'
-#
+baseline_file_t='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/test/baseline.txt'
+air_stroke_file_t='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/test/air_stroke.txt'
+light_stroke_file_t='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/test/light_stroke.txt'
+hard_stroke_file_t='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/test/hard_stroke.txt'
+tickle_file_t='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/test/tickle.txt'
+hit_file_t='/Users/saquib/Documents/Research/HRI/HRI_Python/16X10_air/test/hit.txt'
 
+
+#acquiring train data
 g_base,g2_base=extract_baseline(baseline_file) #import x data for baseline
 c_air_stroke,g_air_stroke,g2_air_stroke=extract_data(air_stroke_file) #import x data for air stroke
 c_light_stroke,g_light_stroke,g2_light_stroke=extract_data(light_stroke_file) #import x data for light stroke
@@ -149,15 +164,22 @@ x_total=np.concatenate((x_total,g2_hit), axis=0)
 
 
 #acquiring test data
-#g_base_t,g2_base_t=extract_baseline(baseline_test_file) #import x test data for baseline
-#c_stroke_t,g_stroke_t,g2_stroke_t=extract_data(stroke_test_file) #import x data for stroke
-#c_massage_t,g_massage_t,g2_massage_t=extract_data(massage_test_file) #import x data for massage
-#x_total_t=np.concatenate((g2_base_t[:,:,:,:], g2_stroke_t[:,:,:,:]),axis=0)
-#x_total_t=np.concatenate((x_total_t,g2_massage_t), axis=0)
-#
+g_base_t,g2_base_t=extract_baseline(baseline_file_t) #import x data for baseline
+c_air_stroke_t,g_air_stroke_t,g2_air_stroke_t=extract_data(air_stroke_file_t) #import x data for air stroke
+c_light_stroke_t,g_light_stroke_t,g2_light_stroke_t=extract_data(light_stroke_file_t) #import x data for light stroke
+c_hard_stroke_t,g_hard_stroke_t,g2_hard_stroke_t=extract_data(hard_stroke_file_t)
+c_tickle_t,g_tickle_t,g2_tickle_t=extract_data(tickle_file_t)
+c_hit_t,g_hit_t,g2_hit_t=extract_data(hit_file_t)
+
+x_total_t=np.concatenate((g2_base_t[:,:,:,:], g2_air_stroke_t),axis=0)
+x_total_t=np.concatenate((x_total_t,g2_light_stroke_t), axis=0)
+x_total_t=np.concatenate((x_total_t,g2_hard_stroke_t[50:,:,:,:]), axis=0)
+x_total_t=np.concatenate((x_total_t,g2_tickle_t), axis=0)
+x_total_t=np.concatenate((x_total_t,g2_hit_t), axis=0)
 
 
 
+#generate train labels
 y_base=np.zeros(g2_base[:,:,:,:].shape[0]) #creating y labels for baseline
 y_air_stroke=np.ones(g2_air_stroke.shape[0]) #creating y labels for stroke
 y_light_stroke=2*np.ones(g2_light_stroke.shape[0]) #creating y labels for massage
@@ -172,17 +194,21 @@ y_total=np.append(y_total,y_hit)
 
 y_all = to_categorical(y_total) #one hot encoding
 
-
 #generate test labels
 #
-#y_base_t=np.zeros(g2_base_t[:,:,:,:].shape[0]) #creating y labels for baseline
-#y_stroke_t=np.ones(g2_stroke_t[:,:,:,:].shape[0]) #creating y labels for stroke
-#y_massage_t=2*np.ones(g2_massage_t.shape[0]) #creating y labels for massage
-#y_total_t=np.append(y_base_t,y_stroke_t) #creating final y by adding base and stroke
-#y_total_t=np.append(y_total_t,y_massage_t) #adding in massage
-#
-#y_all_t = to_categorical(y_total_t) #one hot encoding
+y_base_t=np.zeros(g2_base_t[:,:,:,:].shape[0]) #creating y labels for baseline
+y_air_stroke_t=np.ones(g2_air_stroke_t.shape[0]) #creating y labels for stroke
+y_light_stroke_t=2*np.ones(g2_light_stroke_t.shape[0]) #creating y labels for massage
+y_hard_stroke_t=3*np.ones(g2_hard_stroke_t[50:,:,:,:].shape[0])
+y_tickle_t=4*np.ones(g2_tickle_t.shape[0])
+y_hit_t=5*np.ones(g2_hit_t.shape[0])
+y_total_t=np.append(y_base_t,y_air_stroke_t) #creating final y by adding base and stroke
+y_total_t=np.append(y_total_t,y_light_stroke_t) #adding in massage
+y_total_t=np.append(y_total_t,y_hard_stroke_t)
+y_total_t=np.append(y_total_t,y_tickle_t)
+y_total_t=np.append(y_total_t,y_hit_t)
 
+y_all_t = to_categorical(y_total_t) #one hot encoding
 
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -233,16 +259,16 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 #
-history=model.fit(X_train.reshape(X_train.shape[0],5,16,10,1), y_train,
-                  epochs=12,
-                  validation_data = (X_test.reshape(X_test.shape[0],5,16,10,1), y_test))
-#using test data
-
 #history=model.fit(X_train.reshape(X_train.shape[0],5,16,10,1), y_train,
 #                  epochs=12,
-#                  validation_data = (x_total_t.reshape(x_total_t.shape[0],5,16,10,1), y_all_t))
-#
-#
+#                  validation_data = (X_test.reshape(X_test.shape[0],5,16,10,1), y_test))
+#using test data
+
+history=model.fit(X_train.reshape(X_train.shape[0],5,16,10,1), y_train,
+                  epochs=12,
+                  validation_data = (x_total_t.reshape(x_total_t.shape[0],5,16,10,1), y_all_t))
+
+
 
 
 loss = history.history['loss']
@@ -270,6 +296,12 @@ plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
 
+
+pred=model.predict(x_total_t.reshape(x_total_t.shape[0],5,16,10,1))
+
+y_true=np.array(tf.argmax(y_all_t,axis=1))
+y_pred=np.array(tf.argmax(pred, axis=1))
+cm=np.array(tf.math.confusion_matrix(y_true, y_pred))
 
 
 #--------------------------------------------------------------
